9f04ee8f8d83cb407838f1e45db17f2b22da4689
zune <changzunelee@gmail.com>
2022-04-04 23:35:20 +0900

    ex03-02

---
+++ ex00/src/main/java/com/webjjang/board/controller/BoardController.java
@@ -0,0 +1,12 @@
+package com.webjjang.board.controller;
+
+import org.springframework.stereotype.Controller;
+
+//자동으로 생성이 되게하는 어노테이션
+//@Controller - url과 관련, @Service - 처리, @Repository - DAO
+//@Component - 구성, @RestController - url과 관련 순수데이터
+
+@Controller
+public class BoardController {
+
+}
