6505d37f89231876fedf961b086f4d3cc0003890
zune <changzunelee@gmail.com>
2022-04-28 23:44:29 +0900

    spring

---
+++ testPjt01/src/main/java/testPjt01/MainClass.java
@@ -0,0 +1,13 @@
+package testPjt01;
+
+public class MainClass {
+	
+	public static void main(String[] args) {
+		
+		TranspotationWlak transpotationWlak = new TranspotationWlak();
+		transpotationWlak.move();
+		
+		
+	}
+
+}
---
+++ testPjt01/src/main/java/testPjt01/TranspotationWlak.java
@@ -0,0 +1,11 @@
+package testPjt01;
+
+public class TranspotationWlak {
+	
+	public void move() {
+		System.out.println("도보로 이동합니다.");
+	}
+	
+	
+
+}
