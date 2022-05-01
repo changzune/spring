e44ba85024e695f15994ecf4c8e422b4886e0f29
zune <changzunelee@gmail.com>
2022-05-01 22:42:33 +0900

    spring

--- lec11Pjt/src/main/java/com/brms/book/service/BookRegisterService.java
+++ lec11Pjt/src/main/java/com/brms/book/service/BookRegisterService.java
@@ -1,11 +1,13 @@
 package com.brms.book.service;
 
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.brms.book.Book;
 import com.brms.book.dao.BookDao;
 
-public class BookRegisterService {
+public class BookRegisterService implements InitializingBean, DisposableBean {
 
 	@Autowired
 	private BookDao bookDao;
@@ -22,5 +24,16 @@ public class BookRegisterService {
 	
 	public void destroyMethod() {
 		System.out.println("BookRegisterService 빈(Bean)객체 소멸 단계");
+	}
+
+	@Override
+	public void destroy() throws Exception {
+		System.out.println("bean 객체 생성");
+		
+	}
+
+	@Override
+	public void afterPropertiesSet() throws Exception {
+		System.out.println("bean 객체 소멸");
 	}
 }
