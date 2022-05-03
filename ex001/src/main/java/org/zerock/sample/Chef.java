b3b1e1f80fc49962470d1da19a18e025a2fc714d
zune <changzunelee@gmail.com>
2022-05-03 22:56:55 +0900

    spring

---
+++ ex001/src/main/java/org/zerock/sample/Chef.java
@@ -0,0 +1,8 @@
+package org.zerock.sample;
+
+// 자동으로 생성이 되게 하는 어노테이션
+// @Controller -URL과 관련, @Service -처리, @Repository - DAO, @component - 구성,
+// @Restcontroller -url관련 순수데이터 전달할때 사용한다.
+public class Chef {
+
+}
