a2cfdc833a21da3c6281baa95cc4c34eef2e3f84
zune <changzunelee@gmail.com>
2022-05-09 17:26:27 +0900

    spring

---
+++ ex001/src/main/java/org/zerock/image/controller/ImageController.java
@@ -0,0 +1,17 @@
+package org.zerock.image.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.extern.log4j.Log4j;
+
+//자동으로 생성이 되게 하는 어노테이션
+//@Controller - url과 관련, @Service - 처리,  @Repository - DAO, @Component - 구성, @RestController - url과 ajax관련순수
+@Controller
+@RequestMapping("/image")
+@Log4j
+public class ImageController {
+	
+	
+
+}
