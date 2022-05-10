0bac210e8c996d643349cb0fbee11c9216394d42
zune <changzunelee@gmail.com>
2022-05-10 21:56:08 +0900

    spring

--- ex001/src/main/java/org/zerock/image/controller/ImageController.java
+++ ex001/src/main/java/org/zerock/image/controller/ImageController.java
@@ -1,6 +1,7 @@
 package org.zerock.image.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import lombok.extern.log4j.Log4j;
@@ -12,6 +13,9 @@ import lombok.extern.log4j.Log4j;
 @Log4j
 public class ImageController {
 	
-	
+	@GetMapping("/list")
+	public void list() {
+		log.info("-----[이미지 게시판 리스트]-----------------");
+	}
 
 }
