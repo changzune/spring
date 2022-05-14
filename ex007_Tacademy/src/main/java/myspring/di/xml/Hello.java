432bcf4a50774419a4bebac7d3f91756b3276329
zune <changzunelee@gmail.com>
2022-05-14 22:34:48 +0900

    spring

---
+++ ex007_Tacademy/src/main/java/myspring/di/xml/Hello.java
@@ -0,0 +1,19 @@
+package myspring.di.xml;
+
+public class Hello {
+	private String name;
+	private Printer printer;
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public void setPrinter(Printer printer) {
+		this.printer = printer;
+	}
+	
+	
+	
+	
+
+}
