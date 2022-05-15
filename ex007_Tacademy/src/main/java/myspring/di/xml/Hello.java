74b2f2b4c7b262a4255ae845e305791f91407460
zune <changzunelee@gmail.com>
2022-05-16 05:39:53 +0900

    spring

--- ex007_Tacademy/src/main/java/myspring/di/xml/Hello.java
+++ ex007_Tacademy/src/main/java/myspring/di/xml/Hello.java
@@ -1,8 +1,31 @@
 package myspring.di.xml;
 
+import java.util.List;
+
 public class Hello {
 	private String name;
 	private printer printer;
+	private List<String> names;
+	
+	public Hello() {
+		// TODO Auto-generated constructor stub
+	}
+	
+
+	public List<String> getNames() {
+		return names;
+	}
+
+	public void setNames(List<String> names) {
+		this.names = names;
+	}
+
+
+	public Hello(String name, myspring.di.xml.printer printer) {
+		super();
+		this.name = name;
+		this.printer = printer;
+	}
 
 	public void setName(String name) {
 		this.name = name;
